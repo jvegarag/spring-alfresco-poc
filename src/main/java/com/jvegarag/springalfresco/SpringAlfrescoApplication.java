d79867c693fcd338867c09e7bc4f67ff526bc918
jvegarag <j.vegaragarcia@gmail.com>
2018-05-09 16:18:00 +0200

    - Project creation

---
+++ src/main/java/com/jvegarag/springalfresco/SpringAlfrescoApplication.java
@@ -0,0 +1,12 @@
+package com.jvegarag.springalfresco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAlfrescoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAlfrescoApplication.class, args);
+	}
+}
